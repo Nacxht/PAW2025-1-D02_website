53019ab2d71ecf40a19224e70223b6b9ba301677
Nacxht <zombiecrafter069@gmail.com>
2025-11-18 19:08:08 +0700

    memperbarui file sql

--- ppdb.sql
+++ ppdb.sql
@@ -1,79 +1,34 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.13-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 18, 2025 at 12:33 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: ppdb
+-- ------------------------------------------------------
+-- Server version	10.11.13-MariaDB-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `ppdb`
+-- Table structure for table `form_pendaftaran`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
-  `username` varchar(15) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` char(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `calon_siswa`
---
-
-CREATE TABLE `calon_siswa` (
-  `id_calon_siswa` int(11) NOT NULL,
-  `username` varchar(15) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` char(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orang_tua`
---
-
-CREATE TABLE `orang_tua` (
-  `id_orang_tua` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `jenis_pekerjaan` varchar(30) DEFAULT NULL,
-  `pekerjaan` varchar(30) DEFAULT NULL,
-  `pendapatan` int(11) NOT NULL,
-  `jenis_orang_tua` enum('ayah','ibu','wali') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pendaftaran`
---
-
-CREATE TABLE `pendaftaran` (
-  `id_pendaftaran` int(11) NOT NULL,
-  `id_calon_siswa` int(11) NOT NULL,
-  `id_orang_tua` int(11) NOT NULL,
+DROP TABLE IF EXISTS `form_pendaftaran`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `form_pendaftaran` (
+  `id_form_pendaftaran` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
   `nik` char(16) NOT NULL,
-  `jenis_kelamin` enum('l','p') NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
   `tempat_lahir` varchar(20) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `asal_sekolah` varchar(100) NOT NULL,
@@ -82,83 +37,90 @@ CREATE TABLE `pendaftaran` (
   `rapor` varchar(100) NOT NULL,
   `surat_keterangan_lulus` varchar(100) NOT NULL,
   `surat_kesehatan` varchar(100) NOT NULL,
-  `pasfoto` varchar(100) NOT NULL,
-  `persetujuan_tidak_membawa_hp` enum('true','false') NOT NULL,
-  `persetujuan_asrama` enum('true','false') NOT NULL,
+  `pasfoto_3x4` varchar(100) NOT NULL,
+  `persetujuan_tidak_membawa_hp` enum('false','true') NOT NULL,
+  `persetujuan_asrama` enum('false','true') NOT NULL,
   `status` enum('tertunda','diterima','ditolak') NOT NULL,
-  `nama_lengkap` varchar(100) NOT NULL
+  PRIMARY KEY (`id_form_pendaftaran`),
+  UNIQUE KEY `form_pendaftaran_unique` (`id_user`),
+  CONSTRAINT `form_pendaftaran_users_FK` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `form_pendaftaran`
 --
 
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
+LOCK TABLES `form_pendaftaran` WRITE;
+/*!40000 ALTER TABLE `form_pendaftaran` DISABLE KEYS */;
+/*!40000 ALTER TABLE `form_pendaftaran` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for table `calon_siswa`
---
-ALTER TABLE `calon_siswa`
-  ADD PRIMARY KEY (`id_calon_siswa`);
-
---
--- Indexes for table `orang_tua`
---
-ALTER TABLE `orang_tua`
-  ADD PRIMARY KEY (`id_orang_tua`);
-
---
--- Indexes for table `pendaftaran`
---
-ALTER TABLE `pendaftaran`
-  ADD PRIMARY KEY (`id_pendaftaran`),
-  ADD KEY `pendaftaran_ibfk_1` (`id_calon_siswa`),
-  ADD KEY `id_orang_tua` (`id_orang_tua`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Table structure for table `orang_tua`
 --
 
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+DROP TABLE IF EXISTS `orang_tua`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orang_tua` (
+  `id_orang_tua` int(11) NOT NULL AUTO_INCREMENT,
+  `id_form_pendaftaran` int(11) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `pendapatan` int(11) NOT NULL,
+  `jenis_orang_tua` enum('ayah','ibu','wali') NOT NULL,
+  PRIMARY KEY (`id_orang_tua`),
+  KEY `orang_tua_form_pendaftaran_FK` (`id_form_pendaftaran`),
+  CONSTRAINT `orang_tua_form_pendaftaran_FK` FOREIGN KEY (`id_form_pendaftaran`) REFERENCES `form_pendaftaran` (`id_form_pendaftaran`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- AUTO_INCREMENT for table `calon_siswa`
---
-ALTER TABLE `calon_siswa`
-  MODIFY `id_calon_siswa` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orang_tua`
---
-ALTER TABLE `orang_tua`
-  MODIFY `id_orang_tua` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pendaftaran`
---
-ALTER TABLE `pendaftaran`
-  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
+-- Dumping data for table `orang_tua`
 --
 
---
--- Constraints for table `pendaftaran`
---
-ALTER TABLE `pendaftaran`
-  ADD CONSTRAINT `pendaftaran_ibfk_1` FOREIGN KEY (`id_calon_siswa`) REFERENCES `calon_siswa` (`id_calon_siswa`),
-  ADD CONSTRAINT `pendaftaran_ibfk_2` FOREIGN KEY (`id_orang_tua`) REFERENCES `orang_tua` (`id_orang_tua`);
-COMMIT;
+LOCK TABLES `orang_tua` WRITE;
+/*!40000 ALTER TABLE `orang_tua` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orang_tua` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` char(60) NOT NULL,
+  `role` enum('calon_siswa','admin') NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'ppdb'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-18 19:07:05
