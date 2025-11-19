bb0324de677cfb108d15c30c53d5618a0a04da81
Nacxht <zombiecrafter069@gmail.com>
2025-11-19 16:00:45 +0700

    menambahkan validator untuk email

--- validators/register_validator.php
+++ validators/register_validator.php
@@ -1,47 +1,47 @@
 <?php
 
-require_once 'base_validator.php';
+require_once "base_validator.php";
 
 function validateUsername(string $field, array &$errors): void
 {
     if (cekFieldKosong($field)) {
-        $errors['username'][] = 'Username Wajib diisi';
+        $errors["username"][] = "Username Wajib diisi";
     }
 
     if (!cekAlphaNumeric($field)) {
-        $errors['username'][] = 'Username hanya bisa alpa numerik';
+        $errors["username"][] = "Username hanya bisa alpa numerik";
     }
 
     if (strlen($field) < 3) {
-        $errors['username'][] = 'Panjang Username minimal 3';
+        $errors["username"][] = "Panjang Username minimal 3";
     }
 }
 
 function validateEmail(string $field, array &$errors): void
 {
     if (cekFieldKosong($field)) {
-        $errors['email'][] = 'Email Wajib diisi';
+        $errors["email"][] = "Email wajib diisi!";
+    }
+
+    if (!filter_var($field, FILTER_VALIDATE_EMAIL)) {
+        $errors["email"][] = "Email tidak valid!";
     }
 }
 
-function validatePassword(string $field, string $passwordField, array &$errors): void
+function validatePassword(string $field, array &$errors): void
 {
     if (cekFieldKosong($field)) {
-        $errors['password'][] = 'Password Wajib diisi';
+        $errors["password"][] = "Password Wajib diisi";
     }
 
     if (strlen($field) < 8) {
-        $errors['password'][] = 'Password minimal 8';
-    }
-
-    if ($field !== $passwordField) {
-        $errors['password'][] = 'Password tidak sama';
+        $errors["password"][] = "Password minimal 8";
     }
 }
 
 function validateKonfirmasiPassword(string $field, string $passwordField, array &$errors): void
 {
     if ($passwordField !== $field) {
-        $errors['konfirmasi-password'][] = 'Password tidak sama';
+        $errors["konfirmasi-password"][] = "Password tidak sama";
     }
 }
